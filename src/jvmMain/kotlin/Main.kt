7c74bfe7e30df5f128a4504441e624752ce784ad
makiritare <blacktower.ashaman@gmail.com>
2023-04-16 17:28:26 -0300

    added cancel button and cleaner ui

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,18 +1,20 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import java.io.File
@@ -25,94 +27,113 @@ import java.net.URL
 @Preview
 fun app() {
     MaterialTheme {
-        val colorBack = 0x00FFFF
+        val colorBack = 0xffffff
         var linkOfBook by remember { mutableStateOf("") }
         var titleOfBook by remember { mutableStateOf("") }
         var downloadClicked by remember { mutableStateOf(false) }
         var mp3List by remember { mutableStateOf(listOf<String>()) }
         var error by remember { mutableStateOf(false) }
         var downloadProgress by remember { mutableStateOf(mapOf<String, Float>()) }
+        var isDownloading by remember { mutableStateOf(false) }
+        val downloadJob = remember { mutableStateOf<Job?>(null) }
 
-        Column(
-            modifier = Modifier.background(Color(color = colorBack)).fillMaxHeight().fillMaxWidth()
+        Box(
+            modifier = Modifier.background(Color(color = colorBack)).fillMaxHeight().fillMaxWidth().clip(RoundedCornerShape(50.dp))
         ) {
-            OutlinedTextField(
-                value = linkOfBook,
-                onValueChange = { linkOfBook = it },
-                label = { Text(text = "Link of the Book to download") },
-                placeholder = {},
-                modifier = Modifier.padding(16.dp).fillMaxWidth()
-            )
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
                 OutlinedTextField(
-                    value = titleOfBook,
-                    onValueChange = { titleOfBook = it },
-                    label = { Text(text = "Folder Name") },
+                    value = linkOfBook,
+                    onValueChange = { linkOfBook = it },
+                    label = { Text(text = "Link of the Book to download") },
                     placeholder = {},
-                    modifier = Modifier.padding(16.dp).width(450.dp)
+                    modifier = Modifier.padding(16.dp).fillMaxWidth()
                 )
-                ListFilesButton(
-                    onClick = {
-                        if (isValidUrl(linkOfBook)) {
-                            downloadClicked = true
-                            error = false
-                        } else {
-                            error = true
-                        }
-                    },
-                    modifier = Modifier.padding(top = 24.dp, end = (16.dp)).height(56.dp)
-                )
-            }
-            if (error) {
-                Text(
-                    text = "Please enter a valid URL",
-                    color = Color.Red,
-                    modifier = Modifier.padding(16.dp)
-                )
-            } else if (downloadClicked) {
-                GlobalScope.launch(Dispatchers.IO) {
-                    mp3List = getMp3FromWebPage(linkOfBook)
-                }
-                LazyColumn(
-                    modifier = Modifier.padding(16.dp)
-                        .height(300.dp)
-                        .fillMaxWidth()
-                        .background(Color.Gray)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    items(mp3List) { file ->
-                        FileToDownload(file, downloadProgress[file] ?: 0f)
+                    OutlinedTextField(
+                        value = titleOfBook,
+                        onValueChange = { titleOfBook = it },
+                        label = { Text(text = "Folder Name") },
+                        placeholder = {},
+                        modifier = Modifier.padding(16.dp).width(450.dp)
+                    )
+                    ListFilesButton(
+                        onClick = {
+                            if (isValidUrl(linkOfBook)) {
+                                downloadClicked = true
+                                error = false
+                            } else {
+                                error = true
+                            }
+                        },
+                        modifier = Modifier.padding(top = 24.dp, end = (16.dp)).height(56.dp)
+                    )
+                }
+                if (error) {
+                    Text(
+                        text = "Please enter a valid URL",
+                        color = Color.Red,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                } else if (downloadClicked) {
+                    GlobalScope.launch(Dispatchers.IO) {
+                        mp3List = getMp3FromWebPage(linkOfBook)
+                    }
+                    LazyColumn(
+                        modifier = Modifier.padding(16.dp)
+                            .height(300.dp)
+                            .fillMaxWidth()
+                            .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+                            .padding(8.dp)
+                    ) {
+                        items(mp3List) { file ->
+                            FileToDownload(file, downloadProgress[file] ?: 0f)
+                        }
                     }
                 }
             }
             DownloadButton(
                 onClick = {
-                    GlobalScope.launch(Dispatchers.IO) {
-                        downloadMp3Files(mp3List, titleOfBook) { url, progress ->
-                            downloadProgress = downloadProgress.toMutableMap().apply {
-                                this[url] = progress
+                    if (!isDownloading) {
+                        isDownloading = true
+                        downloadJob.value = GlobalScope.launch(Dispatchers.IO) {
+                            downloadMp3Files(this, mp3List, titleOfBook) { url, progress ->
+                                downloadProgress = downloadProgress.toMutableMap().apply {
+                                    this[url] = progress
+                                }
                             }
                         }
+                    } else {
+                        downloadJob.value?.cancel()
+                        isDownloading = false
                     }
                 },
-                modifier = Modifier.padding(bottom = 20.dp)
+                isDownloading = isDownloading,
+                modifier = Modifier.padding(bottom = 20.dp).align(Alignment.BottomCenter)
+
             )
         }
     }
 }
 
-    //DownloadButton
 @Composable
-    fun DownloadButton(
-        onClick: () -> Unit,
-        modifier: Modifier = Modifier
+fun DownloadButton(
+    onClick: () -> Unit,
+    isDownloading: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
     ) {
-        Button(
-            onClick = onClick,
-            modifier = Modifier.padding(16.dp).fillMaxWidth()
-        ) {
-            Text("Download Book")
-        }
+        Text(if (isDownloading) "Cancel Download" else "Download Book")
     }
+}
+
 
 @Composable
 fun FileToDownload(
@@ -120,9 +141,12 @@ fun FileToDownload(
     progress: Float,
     modifier: Modifier = Modifier
 ) {
-    Row(modifier = modifier.padding(4.dp)) {
+    Row(
+        modifier = modifier.padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically // Add this line to center the elements vertically
+    ) {
         Text(getSecondToLastElement(file) + "_" + file.substringAfterLast("/"))
-        Spacer(modifier = Modifier.width(2.dp))
+        Spacer(modifier = Modifier.width(8.dp))
         LinearProgressIndicator(
             progress = progress,
             modifier = Modifier.weight(1f).height(12.dp)
@@ -176,6 +200,7 @@ fun getMp3FromWebPage(linkOfBook: String): List<String> {
 }
 
 fun downloadMp3Files(
+    scope: CoroutineScope,
     mp3List: List<String>,
     titleOfBook: String,
     onProgress: (String, Float) -> Unit
@@ -196,12 +221,18 @@ fun downloadMp3Files(
         var count: Int
         val fileSize = connection.contentLengthLong
         while (input.read(data).also { count = it } != -1) {
+            if (!scope.isActive) {
+                break
+            }
             total += count.toLong()
             output.write(data, 0, count)
             onProgress(mp3, total.toFloat() / fileSize.toFloat())
         }
         output.close()
         input.close()
+        if (!scope.isActive) {
+            break
+        }
     }
 }
 
